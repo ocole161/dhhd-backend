41c8cd9249eacd255eef807929fc78523a2b102d
Owen <ocole161@gmail.com>
2023-03-28 16:34:52 -0600

    sessions debugging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:username])
         if user&.authenticate(params[:password])
+            puts user
+            puts user.id
             session[:user_id] = user.id
             puts session[:user_id]
             render json: user, status: :ok
