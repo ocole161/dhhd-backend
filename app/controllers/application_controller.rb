eda8c3d551c53ffabce2615f76ce674004f02dd9
Owen <ocole161@gmail.com>
2023-03-28 14:16:03 -0600

    testing in prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
     private
   
     def current_user
-      user = User.find_by(id:session[:user_id])
+      user = User.find_by(id: session[:user_id])
       user
     end
   
